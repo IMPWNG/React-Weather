3a57021207de533375f31015a21548ec618df57c
IMPWONG <matisse.acheen.pro@gmail.com>
2020-07-08 14:59:13 -0400

    Try to connect the API data

---
+++ app.js
@@ -0,0 +1,34 @@
+const api = {
+    key: "653c24e6dea8feb9a6b4024fce11f04a",
+    baseurl:"http://api.openweathermap.org/data/2.5/"
+
+}
+
+
+
+
+
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+
+
+function setQuery(evt) {
+    if(evt.keyCode == 13){
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+
+    }
+}
+
+function getResults(query){
+    fetch(`${api.base}weather?q=${query}&unit=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
+}
